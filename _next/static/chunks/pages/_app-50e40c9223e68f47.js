280a657cb0961654bfb241bfad16325db119fba0
FelixWaweru <wawerufelix60@gmail.com>
2023-05-19 22:33:15 +0000

    Deploying to gh-pages from @ FelixWaweru/who-am-i@28f27cf5fbda9a4a4be7f1007805c5cd75fbc15c 🚀

---
+++ _next/static/chunks/pages/_app-50e40c9223e68f47.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{2427:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(2852)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[179],function(){return _(2427),_(495)}),_N_E=n.O()}]);
