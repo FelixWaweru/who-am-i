6caa7b0d6521241e89dd46522fabddb86b5bb21f
FelixWaweru <wawerufelix60@gmail.com>
2023-05-19 20:55:46 +0000

    Deploying to gh-pages from @ FelixWaweru/who-am-i@e430d7f4d6c0f4ce5ec24185e5056f07fddbb271 🚀

---
+++ _next/static/8LTMafXvy6vD8Wwh_ilrP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-04b0c15553ed30f0.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
