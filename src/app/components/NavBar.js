0d97d99dfecd223b236e6400c4f5b4819a4c0d0d
unknown <wawerufelix60@gmail.com>
2023-05-20 02:27:36 +0300

    feat: resume link

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
         <Link href="#about" className="cta-btn">About Me</Link>
         <Link href="#projects" className="cta-btn">Projects</Link>
         <Link href="#blog" className="cta-btn">Blog</Link>
-        <Link href="" className="cta-btn">Resume</Link>
+        <Link href="https://drive.google.com/file/d/12ZHd3pUhWK4GhqErcU0E0Kq1eqdGm3IC/view?usp=share_link" target="_blank" className="cta-btn">Resume</Link>
       </div>
     </div>
   )
