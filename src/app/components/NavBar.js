c102779b630f8ba49c2ae583e4cf1112a04c1d4a
unknown <wawerufelix60@gmail.com>
2023-05-22 00:02:26 +0300

    feat: mobile header

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -12,10 +12,10 @@ const Navbar = () => {
       </div>
 
       <div className="nav-container-buttons">
-        <a href="#about" className="cta-btn">About Me</a>
-        <a href="#projects" className="cta-btn">Projects</a>
-        <a href="#blog" className="cta-btn">Blog</a>
-        <a href="https://drive.google.com/file/d/12ZHd3pUhWK4GhqErcU0E0Kq1eqdGm3IC/view?usp=share_link" target="_blank" className="cta-btn">Resume</a>
+        <a href="#about" className="mx-1 cta-btn responsive-nav">About Me</a>
+        <a href="#projects" className="mx-1 cta-btn responsive-nav">Projects</a>
+        <a href="#blog" className="mx-1 cta-btn responsive-nav">Blog</a>
+        <a href="https://drive.google.com/file/d/12ZHd3pUhWK4GhqErcU0E0Kq1eqdGm3IC/view?usp=share_link" target="_blank" className="mx-1 cta-btn">Resume</a>
       </div>
     </div>
   )
