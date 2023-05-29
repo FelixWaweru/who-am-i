c28d778e70afedba0a778555631701c955ea0663
Felix Waweru <wawerufelix60@gmail.com>
2023-05-29 11:36:09 +0000

    feat: updated resume link

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
         <a href="#about" className="mx-1 cta-btn responsive-nav">About Me</a>
         <a href="#projects" className="mx-1 cta-btn responsive-nav">Projects</a>
         <a href="#blog" className="mx-1 cta-btn responsive-nav">Blog</a>
-        <a href="https://drive.google.com/file/d/12ZHd3pUhWK4GhqErcU0E0Kq1eqdGm3IC/view?usp=share_link" target="_blank" className="mx-1 cta-btn">Resume</a>
+        <a href="https://drive.google.com/file/d/137j8qTNUTnq3uh5emHAl0SBUSMrp9bOO/view?usp=share_link" target="_blank" className="mx-1 cta-btn">Resume</a>
       </div>
     </div>
   )
