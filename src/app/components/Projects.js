04846fb0de29b1b492c23b501274457e09f306bb
Felix Waweru <wawerufelix60@gmail.com>
2023-11-24 12:09:42 +0000

    feat: updated project

--- src/app/components/Projects.js
+++ src/app/components/Projects.js
@@ -19,46 +19,46 @@ const Projects = async () => {
     const projectData = [
         {
             id: 1,
+            title: 'Code Fundi',
+            description:
+            'Code Fundi is an AI coding assistant that helps developers write better code faster. With our service, you can cut down development time by eliminating the need to manually browse the web looking for solutions and instead, have the solutions generated directly in your code editor.',
+            gitHubLink: 'https://github.com/code-Fundi',
+        },
+        {
+            id: 2,
             title: 'Elevenlabs NPM Package',
             description:
             'A JavaScript based, open source NPM package that wraps around the Elevenlabs public API and is used to convert text to speech.',
             gitHubLink: 'https://www.npmjs.com/package/elevenlabs-node',
         },
         {
-            id: 2,
+            id: 3,
             title: 'TiktokChatGPT',
             description:
             'This is a conversational bot for tiktok livestreams built on ChatGPT. It uses the OpenAI API to generate responses, turns the responses into a vocal audio response and then responds to live stream events using the generated audio.',
             gitHubLink: 'https://github.com/FelixWaweru/TiktokChatGPT',
         },
         {
-            id: 3,
+            id: 4,
             title: 'M-Kono Prosthetic Hand',
             description:
             'A prosthetic hand controlled using an Android Application and an Arduino board. The project entailed receiving and interpreting vocal user commands using an Android application and converting it into hand motions using an Arduino board, servos and a 3D printed prosthetic hand. It was developed for my fourth year project.',
             gitHubLink: 'https://github.com/FelixWaweru/M-Kono',
         },
         {
-            id: 4,
+            id: 5,
             title: 'MHENGA',
             description:
             'I developed a Twitter bot that shares Swahili proverbs and their translations as tweets. In the spirit of staying authentic to my roots, I chose to create the bot using a Swahili programming language called Swahili-lang and a little bit of JavaScript to help with the tweet posting.',
             gitHubLink: 'https://github.com/FelixWaweru/MHENGA',
         },
         {
-            id: 5,
+            id: 6,
             title: 'Compliment Bot',
             description:
             'A Python based Twitter bot that posts motivational tweets and automatically responds to users live on Twitter. The bot also sends direct messages and personalised tweets to its followers. ',
             gitHubLink: 'https://github.com/FelixWaweru/ComplimentBot',
-        },
-        {
-            id: 6,
-            title: 'WikiGPT',
-            description:
-            'This is a Next.JS app that uses the OpenAI API to generate responses using ChatGPT. I developed the app to familiarise myself with Next.JS and OpenAI technologies and it is hosted on Vercel with CI/CD deployments setup for fast feature rollouts.',
-            gitHubLink: 'https://wikigpt.vercel.app/',
-        },
+        }
     ];
 
     return (
